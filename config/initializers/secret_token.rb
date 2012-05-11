b6f64c8ca608df130615b511913fd59059bb899c
ICPU Chris <dev@icpu.co.uk>
2012-05-11 22:14:02 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yabe::Application.config.secret_token = '25fd4b6baf7f83f6911e1db592400f2437a328bb71ab1bff27379ec9e2402eb138f89caba74d52650c18f5887c1d1c9cc3be99d6859f82d6f95b275cc5b0b225'
