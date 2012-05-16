536fbe033ffa1b76781a3b1b94f252364a109cd4
Chris Potts <dev@icpu.co.uk>
2012-05-16 18:36:13 +0100

    userediting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+   before_filter :correct_user,   only: [:edit, :update]
+  
   def new
    @user = User.new
   end
@@ -19,11 +22,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+  end
+
+  def index
+  @users = User.paginate(page: params[:page])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
@@ -32,4 +37,16 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end 
+
+  private
+
+    def signed_in_user
+       store_location
+       redirect_to signin_path, notice: "Please sign in." unless signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,11 +17,24 @@ module SessionsHelper
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
 #    @current_user ||= user_from_remember_token
   end
+ 
+  def current_user?(user)
+    user == current_user
+  end
 
   def sign_out
     session[:user_id] = nil
     self.current_user = nil
 #    cookies.delete(:remember_token)
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
   end
 
   private
@@ -30,4 +43,9 @@ module SessionsHelper
 #      remember_token = cookies[:remember_token]
 #      User.find_by_remember_token(remember_token) unless remember_token.nil?
 #    end
+
+  def clear_return_to
+      session.delete(:return_to)
+    end
+
 end
