b18a51ca0c13a9ad49d77981287e3c523eecf971
Chris Potts <dev@icpu.co.uk>
2012-05-20 15:18:59 +0100

    refactor secrets

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Yabe::Application.config.secret_token = '25fd4b6baf7f83f6911e1db592400f2437a328bb71ab1bff27379ec9e2402eb138f89caba74d52650c18f5887c1d1c9cc3be99d6859f82d6f95b275cc5b0b225'
+Yabe::Application.config.secret_token = APP_CONFIG[:secret_token]
